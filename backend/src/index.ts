f6d376ae69940219258c0e44fd482dc3cb21fa57
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2026-06-18 21:07:17 +0700

    feat: add env, for api url(FE) and port(BE)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,21 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import cors from 'cors';
 import { errorHandler } from "#middlewares/errorHandler.js";
 import taskRoutes from "#routes/taskRoutes.js";
 
+dotenv.config();
+
 const app = express();
-const PORT = 5001;
 
 app.use(cors());
 app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
 app.use('/api', taskRoutes);
 
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-    console.log(`Backend running on http://localhost:${PORT}`);
+    console.log(`Server is running dynamically on port ${PORT}`);
 });
